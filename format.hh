b75adf5665d37547ba02a53c7aee4a4cef99a23d
Aaron McDaid <aaronmcdaid@gmail.com>
2017-05-10 15:18:42 +0200

    format.hh

---
+++ format.hh
@@ -0,0 +1,6 @@
+/* A simple string formatting library, loosely based on that of C#, but using
+ * a compile-time string to be type-safe and efficient.
+ *
+ * I'll start with a simple run-time implementation first, as it's easier
+ * to develop and test.
+ */
